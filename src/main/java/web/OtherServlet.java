5e97e9b8bf71082e49b1455c944b50a5a8670f3b
charroux <benoit.charroux@free.fr>
2015-03-09 10:43:43 +0100

    ajout d une deuxieme servlet et d une JSP

--- src/main/java/web/OtherServlet.java
+++ src/main/java/web/OtherServlet.java
@@ -10,27 +10,29 @@ import javax.servlet.http.HttpServletResponse;
 import model.Person;
 
 
-public class AddPersonServlet extends HttpServlet {
+public class OtherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+		String a = request.getParameter("age");
+		int age = Integer.parseInt(a);
+		
+		System.out.println("Je suis dans la deuxieme servlet OtherServlet : name="+name + " age=" + age + ", et j'affiche une JSP");
+		
+		
+		request.setAttribute("person", new Person(name, age));
+		request.getRequestDispatcher("/confirmation.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name = request.getParameter("name");
-		String a = request.getParameter("age");
-		int age = Integer.parseInt(a);
 		
-		System.out.println("Je suis dans le premiere servlet AddPersonServlet : name="+name + " age=" + age);
-		
-		response.sendRedirect("otherServlet?name=" + name + "&age=" + age);
 	}
 
 }
